45f3c6e7120e6057dd36cb382fa57e3321d9f030
Orjan Monsen <mons@dal.ca>
2017-03-23 13:43:14 -0300

    Added request control blocks

--- sws.c
+++ sws.c
@@ -14,7 +14,64 @@
 
 #include "network.h"
 
+/* constants */
 #define MAX_HTTP_SIZE 8192                 /* size of buffer to allocate */
+#define MAX_REQUESTS 100                   /* max # of requests          */
+#define TRUE 1
+#define FALSE 0
+
+
+/* request control block */
+struct rcb {
+    int sequence_number;         /* Sequence number of the request                         */
+    int client_file_descriptor;  /* File descriptor of the client                          */
+    int bytes_remaining;         /* # of bytes that remain to be sent                      */
+    int quantum;                 /* Max # of bytes to be sent when the request is serviced */
+    FILE* file;                  /* File handle of the requested file                      */
+    struct rcb* next_block;             /* Pointer to the next block in line                      */
+};
+
+
+/* global variables */
+static struct rcb request_table[MAX_REQUESTS];
+static struct rcb *free_rcb;
+
+/**
+ * Function that initializes the request control block
+ */
+void rcb_init() {
+    free_rcb = request_table;
+    //init sequence numbers in the request table:
+    for (int i = 0; i < MAX_REQUESTS - 1; i++) {
+        request_table[i].sequence_number = i+1;
+        request_table[i].next_block = &request_table[i+1];
+    }
+}
+
+/**
+ * Function that creats and allocates a new request control block
+ */
+struct rcb *rcb_alloc(void) {
+    struct rcb *block;
+
+    //TODO: Mutex lock for multithreading
+    block = free_rcb;                       // allocate
+    free_rcb = free_rcb->next_block;
+
+    //set block's memory location to 0's to make space for the new block:
+    memset(block, 0, sizeof(struct rcb));
+
+    return block;
+}
+
+/**
+ * Function that frees the request control block
+ */
+void rcb_free(struct rcb *block) {
+    //TODO: Mutex lock for multithreading
+    block->next_block = free_rcb;
+    free_rcb = block;
+}
 
 
 /* This function takes a file handle to a client, reads in the request,
@@ -83,7 +140,7 @@ static void serve_client( int fd ) {
       fclose( fin );
     }
   }
-  close( fd );                                     /* close client connectuin*/
+  close( fd );                                     /* close client connection*/
 }
 
 
@@ -98,27 +155,33 @@ static void serve_client( int fd ) {
  * Returns: an integer status code, 0 for success, something else for error.
  */
 int main(int argc, char **argv) {
-  int port = -1;                                    /* server port # */
-  int fd;                                           /* client file descriptor */
-  int numThreads;
-  int cacheSize;
+    int port = -1;                                    /* server port # */
+    int fd;                                           /* client file descriptor */
+    int numThreads;
+    int cacheSize;
+    struct rcb *request;
 
-  /* check for and process input parameters: */
-  if((argc <= 4) || (sscanf(argv[1], "%d", &port) < 1)
-                 || (sscanf(argv[3], "%d", &numThreads) < 1)
-                 || (sscanf(argv[4], "%d", &cacheSize) < 1)) {
-    printf("usage: sws <port> <schedulerAlgorithm> <numThreads> <cacheSize>\n");
-    return 0;
-  }
-
-  /* initialize all components: */
-  network_init(port);                             /* init network module */
-
-  for( ;; ) {                                       /* main loop */
-    network_wait();                                 /* wait for clients */
-
-    for(fd = network_open(); fd >= 0; fd = network_open()) { /* get clients */
-      serve_client( fd );                           /* process each client */
+    /* check for and process input parameters: */
+    if((argc <= 4) || (sscanf(argv[1], "%d", &port) < 1)
+                   || (sscanf(argv[3], "%d", &numThreads) < 1)
+                   || (sscanf(argv[4], "%d", &cacheSize) < 1)) {
+        printf("usage: sws <port> <schedulerAlgorithm> <numThreads> <cacheSize>\n");
+        return 0;
     }
-  }
+
+    /* initialize all components: */
+    network_init(port);                /* init network module */
+    rcb_init();                        /* init rcb */
+
+
+
+    for( ;; ) {                                                  /* main loop */
+        network_wait();                                          /* wait for clients */
+
+        for(fd = network_open(); fd >= 0; fd = network_open()) { /* get clients */
+            request = rcb_alloc();
+            request->client_file_descriptor = fd;
+            serve_client(fd);                                  /* process each client */
+        }
+    }
 }

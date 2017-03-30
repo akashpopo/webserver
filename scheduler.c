fabfc6ff6135445acb0165c5abdaf4bc36dc539a
Orjan Monsen <mons@dal.ca>
2017-03-29 23:36:59 -0300

    refactored scheduler_init function

--- scheduler.c
+++ scheduler.c
@@ -11,7 +11,7 @@ extern struct scheduler_info round_robin_scheduler;
 extern struct scheduler_info multilevel_scheduler;
 
 /* setup an array of all supported schedulers */
-static struct scheduler_info* supported_schedulers[] = {
+static struct scheduler_info* supported_scheduler_algorithms[] = {
     &shortest_job_first_scheduler,
     &round_robin_scheduler,
     &multilevel_scheduler
@@ -22,12 +22,16 @@ static struct scheduler_info* selected_scheduler;
 
 /* init and set the scheduler */
 extern void scheduler_init(char* selected_algorithm) {
+    // find the scheduler set by user input
     for (int i = 0; i < NUM_SUPPORTED_ALGORITHMS; i++) {
-        if (!strcmp(selected_algorithm, supported_schedulers[i]->algorithm)) {
-            selected_scheduler = supported_schedulers[i];
+        char* curr_algorithm = supported_scheduler_algorithms[i]->algorithm;
+        if (!strcmp(selected_algorithm, curr_algorithm)) {
+            // we found the scheduler algorithm
+            selected_scheduler = supported_scheduler_algorithms[i];
             return;
         }
     }
+    // the algorithm did not match the supported scheduling algorithms
     printf("This webserver does not support the %s scheduling algorithm.\n", selected_algorithm);
     printf("Only SJF, RR, and MLFB are supported. Terminating...\n");
     abort();
--- scheduler.h
+++ scheduler.h
@@ -4,15 +4,17 @@
 #define NUM_SUPPORTED_ALGORITHMS 3
 
 /* function prototypes */
-typedef void (*submit_func)(struct rcb*);
-typedef struct rcb* (*get_next_func)(void);
 extern void scheduler_init(char* selected_algorithm);
 extern void submit_to_scheduler(struct rcb* request_control_block);
 extern struct rcb* get_from_scheduler();
 
+/* pointers to the functions inside the scheduler_info struct */
+typedef void (*submit_func)(struct rcb*);
+typedef struct rcb* (*get_next_func)(void);
+
 /* scheduler struct */
 struct scheduler_info {
-    char* algorithm;                 /* name of the algorithm (e.g. SJF) */
+    char* algorithm;                /* name of the algorithm (e.g. SJF) */
     submit_func submit_rcb;         /* pointer to submit function */
     get_next_func get_next_rcb;     /* pointer to get_next function */
 };
